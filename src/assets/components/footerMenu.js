f160a17fd49c793d1f8ef28610d76e4794600d85
MarkSuttirak <suttirak.ch@zaviago.com>
2023-04-11 17:58:18 +0700

    Update footerMenu.js

--- src/assets/components/footerMenu.js
+++ src/assets/components/footerMenu.js
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGift, faHistory, faHome, faUser } from "@fortawesome/pro-solid-svg-icons";
 
 const FooterMenu = (props) => {
     return (
-        <div>
-            <footer className="p-6 fixed w-full bg-white bottom-0 z-50" style={{borderTop:"1px solid #F8F8F8"}}>
-                <nav>
-                    <ul className="flex justify-around">
-                        <li className={`${(props.activeMenu === 1 ? "primary-color" : "menu-inactive-color")}`}>
-                            <a href="#">หน้าหลัก</a>
-                        </li>
-                        <li className={`${(props.activeMenu === 2 ? "primary-color" : "menu-inactive-color")}`}>
-                            <a href="#">แลกของรางวัล</a>
-                        </li>
-                        <li className={`${(props.activeMenu === 3 ? "primary-color" : "menu-inactive-color")}`}>
-                            <a href="#">ประวัติ</a>
-                        </li>
-                        <li className={`${(props.activeMenu === 4 ? "primary-color" : "menu-inactive-color")}`}>
-                            <a href="#">ข้อมูลของฉัน</a>
-                        </li>
-                    </ul>
-                </nav>
-            </footer>
-        </div>
+        <footer className="px-6 py-4 fixed w-full bg-white bottom-0 z-50" style={{borderTop:"1px solid #F8F8F8"}}>
+            <nav>
+              <ul className="flex justify-around">
+                <li className={`text-sm ${(props.activeMenu === 1 ? "primary-color font-bold" : "menu-inactive-color font-medium")}`}>
+                  <a href="#"><FontAwesomeIcon icon={faHome} className="block text-2xl"/>หน้าหลัก</a>
+                </li>
+                <li className={`text-sm ${(props.activeMenu === 2 ? "primary-color font-bold" : "menu-inactive-color font-medium")}`}>
+                  <a href="#"><FontAwesomeIcon icon={faGift} className="block text-2xl"/>แลกของรางวัล</a>
+                </li>
+                <li className={`text-sm ${(props.activeMenu === 3 ? "primary-color font-bold" : "menu-inactive-color font-medium")}`}>
+                  <a href="#"><FontAwesomeIcon icon={faHistory} className="block text-2xl"/>ประวัติ</a>
+                </li>
+                <li className={`text-sm ${(props.activeMenu === 4 ? "primary-color font-bold" : "menu-inactive-color font-medium")}`}>
+                  <a href="#"><FontAwesomeIcon icon={faUser} className="block text-2xl"/>ข้อมูลของฉัน</a>
+                </li>
+              </ul>
+            </nav>
+        </footer>
     )
 }
 
