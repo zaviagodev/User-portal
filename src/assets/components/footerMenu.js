50c980f5ec2b7457c45fdf40c88ae54654b17c85
MarkSuttirak <suttirak.ch@zaviago.com>
2023-04-11 18:01:28 +0700

    Update footerMenu.js

--- src/assets/components/footerMenu.js
+++ src/assets/components/footerMenu.js
@@ -6,18 +6,30 @@ const FooterMenu = (props) => {
     return (
         <footer className="px-6 py-4 fixed w-full bg-white bottom-0 z-50" style={{borderTop:"1px solid #F8F8F8"}}>
             <nav>
-              <ul className="flex justify-around">
+              <ul className="flex justify-between">
                 <li className={`text-sm ${(props.activeMenu === 1 ? "primary-color font-bold" : "menu-inactive-color font-medium")}`}>
-                  <a href="#"><FontAwesomeIcon icon={faHome} className="block text-2xl"/>หน้าหลัก</a>
+                  <a href="#">
+                    <FontAwesomeIcon icon={faHome} className="block text-xl m-auto"/>
+                    <span className="mt-1 inline-block">หน้าหลัก</span>
+                  </a>
                 </li>
                 <li className={`text-sm ${(props.activeMenu === 2 ? "primary-color font-bold" : "menu-inactive-color font-medium")}`}>
-                  <a href="#"><FontAwesomeIcon icon={faGift} className="block text-2xl"/>แลกของรางวัล</a>
+                  <a href="#">
+                    <FontAwesomeIcon icon={faGift} className="block text-xl m-auto"/>
+                    <span className="mt-1 inline-block">แลกของรางวัล</span>
+                  </a>
                 </li>
                 <li className={`text-sm ${(props.activeMenu === 3 ? "primary-color font-bold" : "menu-inactive-color font-medium")}`}>
-                  <a href="#"><FontAwesomeIcon icon={faHistory} className="block text-2xl"/>ประวัติ</a>
+                  <a href="#">
+                    <FontAwesomeIcon icon={faHistory} className="block text-xl m-auto"/>
+                    <span className="mt-1 inline-block">ประวัติ</span>
+                  </a>
                 </li>
                 <li className={`text-sm ${(props.activeMenu === 4 ? "primary-color font-bold" : "menu-inactive-color font-medium")}`}>
-                  <a href="#"><FontAwesomeIcon icon={faUser} className="block text-2xl"/>ข้อมูลของฉัน</a>
+                  <a href="#">
+                    <FontAwesomeIcon icon={faUser} className="block text-xl m-auto"/>
+                    <span className="mt-1 inline-block">ข้อมูลของฉัน</span>
+                  </a>
                 </li>
               </ul>
             </nav>
