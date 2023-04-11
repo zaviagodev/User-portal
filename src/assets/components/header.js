0706212e4b65d263bb9141f4537eea541161f6d7
MarkSuttirak <suttirak.ch@zaviago.com>
2023-04-11 17:25:15 +0700

    Commit Codes

--- src/assets/components/header.js
+++ src/assets/components/header.js
@@ -4,11 +4,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const HeaderMobile = (props) => {
     return (
         <div>
-            <header className="p-6 flex item-center justify-between">
+            <header className="p-6 flex item-center justify-between" style={{boxShadow:"0 3px 15px 0 #7777771A"}}>
                 <a href={props.firstLink} className="inline">
                     <FontAwesomeIcon icon={props.firstIcon} className="text-2xl"/>
                 </a>
-                <h1 className="font-bold">{props.title}</h1>
+                <h1 className="font-bold text-lg">{props.title}</h1>
                 <a href={props.secondLink} className={`text-2xl ${(props.secondBtn == false ? "invisible" : "")}`}>
                     <FontAwesomeIcon icon={props.secondIcon} className={"text-2xl"}/>
                 </a>
--- src/pages/pointCollection.js
+++ src/pages/pointCollection.js
@@ -7,6 +7,13 @@ const PointCollection = () => {
     return (
         <div>
             <HeaderMobile firstLink="/" firstIcon={faChevronLeft} title="วิธีเก็บคะแนน" secondBtn={false}/>
+            <main className="p-6">
+                <h2 className="font-bold">เงื่อนไขในการให้คะแนน</h2>
+                <ul className="text-sm list-disc mt-3 ps-4" style={{color:"#585858"}}>
+                    <li>ค่าบริการปกติ ทุก 500 บาท มีค่าเท่ากับ 1 คะแนน</li>
+                    <li>ค่าบริการซ่อมสีและตัวถัง (ฺB/P) กรณีประกันจ่าย 1,000 บาท มีค่าเท่ากับ 1 คะแนน</li>
+                </ul>
+            </main>
         </div>
     )
 }
