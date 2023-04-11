d06dac83d7c09b7aed8f6d7428de0ce806fc7d9d
MarkSuttirak <suttirak.ch@zaviago.com>
2023-04-11 12:09:58 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="p-6">
+      <p className='pb-4'>
+        For English, please click <a href="#eng" className='underline'>here</a>
+      </p>
+      <h1 className="text-3xl font-bold">
+        Hallo Welt! Ich bin ein React-App!
+      </h1>
+      <h1 className="text-3xl font-bold pt-96" id="eng">
+        Hello World! I am a React App!
+      </h1>
+    </div>
   )
 }
