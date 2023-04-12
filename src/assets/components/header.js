23c82d640e4e214abc86aff7cea3896fbf37d1b4
MarkSuttirak <suttirak.ch@zaviago.com>
2023-04-12 10:53:19 +0700

    Update header.js

--- src/assets/components/header.js
+++ src/assets/components/header.js
@@ -6,11 +6,11 @@ const HeaderMobile = (props) => {
       <div>
         <header className="p-6 flex items-center justify-between fixed w-full bg-white top-0 z-50" style={{boxShadow:"0 3px 15px 0 #7777771A"}}>
           <a href={props.firstLink} className="inline">
-            <FontAwesomeIcon icon={props.firstIcon} className="text-2xl"/>
+            <FontAwesomeIcon icon={props.firstIcon} className="text-2xl" style={{minWidth:"24px"}}/>
           </a>
-          <h1 className={`font-bold text-lg ${(props.secondBtn == false ? "-ml-3" : "")}`}>{props.title}</h1>
-          <a href={props.secondLink} className={`text-2xl ${(props.secondBtn == false ? "invisible" : "")}`}>
-            <FontAwesomeIcon icon={props.secondIcon} className={"text-2xl"}/>
+          <h1 className={`font-bold text-lg`}>{props.title}</h1>
+          <a href={props.secondLink} className={`text-2xl ${(props.secondBtn == false ? "w-6 invisible" : "w-6")}`}>
+            <FontAwesomeIcon icon={props.secondIcon} className="text-2xl" style={{minWidth:"24px"}}/>
           </a>
         </header>
       </div>
