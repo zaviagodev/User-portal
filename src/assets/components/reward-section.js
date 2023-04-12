04ae2ab54f529d6e7127c932db623bd4cad6da0a
MarkSuttirak <suttirak.ch@zaviago.com>
2023-04-12 10:46:41 +0700

    Commit Codes

--- src/assets/components/reward-section.js
+++ src/assets/components/reward-section.js
@@ -4,6 +4,8 @@ import giftVoucher from "../../assets/images/gift-voucher.svg";
 import brandModulo from "../../assets/images/brand-modulo.svg";
 import neckPillowCat from "../../assets/images/neck-pillow-cat.svg";
 import neckPillowModulo from "../../assets/images/neck-pillow-modulo.svg";
+import pillowModulo from "../../assets/images/pillow-modulo.svg";
+import coolerBag from "../../assets/images/cooler-bag.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
@@ -26,24 +28,24 @@ const RewardSection = () => {
         <>
             <h2 className="font-bold mb-2 px-6">ของรางวัลทั้งหมด (<span id="num-all-items">9</span>)</h2>
             <div className="overflow-scroll flex gap-x-4 flex-nowrap px-6">
+                <CardReward image={coolerBag} name="กระเป๋าเก็บความเย็น ฮอนด้านนทบุรี" point="40" rewardLink="#"/>
                 <CardReward image={giftVoucher} name="คูปองแทนเงินสด มูลค่า 50 บาท" point="10" rewardLink="#"/>
-                <CardReward image={brandModulo} name="ปลอกเข็มขัดนิรภัย Brand Modulo" point="30" rewardLink="#"/>
+                <CardReward image={giftVoucher} name="คูปองแทนเงินสด มูลค่า 200 บาท" point="30" rewardLink="#"/>
+                <CardReward image={giftVoucher} name="คูปองแทนเงินสด มูลค่า 500 บาท" point="50" rewardLink="#"/>
                 <CardReward image={giftVoucher} name="คูปองแทนเงินสด มูลค่า 1,200 บาท" point="100" rewardLink="#"/>
-                <CardReward image={giftVoucher} name="คูปองแทนเงินสด มูลค่า 50 บาท" point="10" rewardLink="#"/>
                 <CardReward image={brandModulo} name="ปลอกเข็มขัดนิรภัย Brand Modulo" point="30" rewardLink="#"/>
-                <CardReward image={giftVoucher} name="คูปองแทนเงินสด มูลค่า 1,200 บาท" point="100" rewardLink="#"/>
-                <CardReward image={giftVoucher} name="คูปองแทนเงินสด มูลค่า 50 บาท" point="10" rewardLink="#"/>
-                <CardReward image={brandModulo} name="ปลอกเข็มขัดนิรภัย Brand Modulo" point="30" rewardLink="#"/>
-                <CardReward image={giftVoucher} name="คูปองแทนเงินสด มูลค่า 1,200 บาท" point="100" rewardLink="#"/>
+                <CardReward image={neckPillowModulo} name="หมอนรองคอ Brand Modulo" point="40" rewardLink="#"/>
+                <CardReward image={neckPillowCat} name="หมอนรองคอน้องแมว ฮอนด้านนทบุรี" point="40" rewardLink="#"/>
+                <CardReward image={pillowModulo} name="หมอนอิง Brand Modulo" point="40" rewardLink="#"/>
             </div>
 
             <h2 className="font-bold mt-8 mb-2 px-6">ของรางวัล (<span id="num-items">5</span>)</h2>
             <div className="overflow-scroll flex gap-x-4 flex-nowrap px-6">
+                <CardReward image={coolerBag} name="กระเป๋าเก็บความเย็น ฮอนด้านนทบุรี" point="40" rewardLink="#"/>
+                <CardReward image={brandModulo} name="ปลอกเข็มขัดนิรภัย Brand Modulo" point="30" rewardLink="#"/>
                 <CardReward image={neckPillowModulo} name="หมอนรองคอ Brand Modulo" point="40" rewardLink="#"/>
                 <CardReward image={neckPillowCat} name="หมอนรองคอน้องแมว ฮอนด้านนทบุรี" point="40" rewardLink="#"/>
-                <CardReward image={giftVoucher} name="คูปองแทนเงินสด มูลค่า 1,200 บาท" point="10" rewardLink="#"/>
-                <CardReward image={neckPillowCat} name="หมอนรองคอน้องแมว ฮอนด้านนทบุรี" point="40" rewardLink="#"/>
-                <CardReward image={giftVoucher} name="คูปองแทนเงินสด มูลค่า 1,200 บาท" point="10" rewardLink="#"/>
+                <CardReward image={pillowModulo} name="หมอนอิง Brand Modulo" point="40" rewardLink="#"/>
             </div>
 
             <h2 className="font-bold mt-8 mb-2 px-6">คูปองแทนเงินสด (<span id="num-coupons">4</span>)</h2>
