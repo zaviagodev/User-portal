d060e43aeb959ecb48cc0f9b25b1e8f5c0b6f8a0
MarkSuttirak <suttirak.ch@zaviago.com>
2023-04-12 14:45:23 +0700

    Commit Codes

--- src/pages/history/history.js
+++ src/pages/history/history.js
@@ -6,21 +6,17 @@ import { faChevronLeft } from "@fortawesome/pro-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const History = () => {
-    const onChange = (key) => {
-        console.log(key);
-    }
-
     const CardHistory = (props) => {
         return (
-          <div className="mb-4">
+          <div className="mb-4 card-history">
             <Card>
               <div className="columns-2 flex items-center justify-between">
-                <div className="block">
-                    <h2 className="text-color font-bold text-base mb-3">{props.title}</h2>
-                    <p className="secondary-color text-xs">{props.date}</p>
+                <div className="block history-title">
+                  <h2 className="text-color font-bold text-base mb-3" style={{overflow:"hidden",textOverflow:"ellipsis"}}>{props.title}</h2>
+                  <p className="secondary-color text-xs">{props.date}</p>
                 </div>
                 <div className="block">
-                    <h2 className="primary-color font-bold">{props.positive === true ? "+" : "-"}{props.point} คะแนน</h2>
+                  <h2 className="primary-color font-bold">{props.positive === true ? "+" : "-"}{props.point} คะแนน</h2>
                 </div>
               </div>
             </Card>
@@ -38,6 +34,11 @@ const History = () => {
                 <CardHistory title="แลกรางวัลเป็นคะแนน" date="22 ม.ค. 2022" point="10" positive={true}/>
                 <CardHistory title="เข้าใช้บริการปกติ" date="22 ม.ค. 2022" point="10" positive={true}/>
                 <CardHistory title="เข้าใช้บริการปกติ" date="22 ม.ค. 2022" point="10" positive={true}/>
+                <CardHistory title="เข้าใช้บริการซ่อมสี" date="22 ม.ค. 2022" point="10" positive={true}/>
+                <CardHistory title="แลกรางวัลเป็นคะแนน" date="22 ม.ค. 2022" point="10" positive={true}/>
+                <CardHistory title="เข้าใช้บริการปกติ" date="22 ม.ค. 2022" point="10" positive={true}/>
+                <CardHistory title="เข้าใช้บริการปกติ" date="22 ม.ค. 2022" point="10" positive={true}/>
+                <CardHistory title="เข้าใช้บริการซ่อมสี" date="22 ม.ค. 2022" point="10" positive={true}/>
               </div>
             )
         },
@@ -49,19 +50,22 @@ const History = () => {
                 <CardHistory title="คูปองแทนเงินสดมูลค่า 50 บาท" date="22 ม.ค. 2022" point="10" positive={false}/>
                 <CardHistory title="คูปองแทนเงินสดมูลค่า 200 บาท" date="22 ม.ค. 2022" point="30" positive={false}/>
                 <CardHistory title="คูปองแทนเงินสดมูลค่า 500 บาท" date="22 ม.ค. 2022" point="50" positive={false}/>
+                <CardHistory title="คูปองแทนเงินสดมูลค่า 1,200 บาท" date="22 ม.ค. 2022" point="100" positive={false}/>
+                <CardHistory title="ปลอกหุ้มเข็มขัด Brand Modulo" date="22 ม.ค. 2022" point="30" positive={false}/>
+                <CardHistory title="หมอนรองคอน้องแมว ฮอนด้า..." date="22 ม.ค. 2022" point="40" positive={false}/>
               </div>
             )
         }
     ]
 
     return (
-        <div>
-            <HeaderMobile firstLink="/" firstIcon={faChevronLeft} title="ประวัติ" secondBtn={false}/>
-            <main className="p-6 tabs-menu" style={{backgroundColor:"#F8F8F8",borderRadius:"12px 12px 0 0"}}>
-                <Tabs defaultActiveKey="1" items={menus} onChange={onChange} centered={true}/>
-            </main>
-            <FooterMenu activeMenu={3}/>
-        </div>
+      <div>
+        <HeaderMobile firstLink="/" firstIcon={faChevronLeft} title="ประวัติ" secondBtn={false}/>
+          <main className="p-6 tabs-menu" style={{backgroundColor:"#F8F8F8",borderRadius:"12px 12px 0 0"}}>
+            <Tabs defaultActiveKey="1" items={menus} centered={true}/>
+          </main>
+        <FooterMenu activeMenu={3}/>
+      </div>
     )
 }
 
