adb1b666f0eff03aabe0d4424719f36737732aca
MarkSuttirak <suttirak.ch@zaviago.com>
2023-04-12 10:55:24 +0700

    Update rewards.js

--- src/pages/points-rewards/rewards.js
+++ src/pages/points-rewards/rewards.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Card, Button, Space } from "antd";
-import { faChevronLeft, faGift, faGiftCard } from "@fortawesome/pro-solid-svg-icons";
+import { faChevronLeft, faGiftCard } from "@fortawesome/pro-solid-svg-icons";
 import FooterMenu from "../../assets/components/footerMenu";
 import HeaderMobile from "../../assets/components/header";
 import RewardSection from "../../assets/components/reward-section";
@@ -8,7 +8,7 @@ import RewardSection from "../../assets/components/reward-section";
 const RewardsPage = () => {
     return (
         <div>
-            <HeaderMobile firstLink="/" firstIcon={faChevronLeft} title="แลกของรางวัล" secondBtn={true} secondLink="#" secondIcon={faGift} />
+            <HeaderMobile firstLink="/" firstIcon={faChevronLeft} title="แลกของรางวัล" secondBtn={true} secondLink="#" secondIcon={faGiftCard} />
             <main>
                 <section className="mt-7 py-6" style={{backgroundColor:"#F8F8F8"}}>
                     <RewardSection />
