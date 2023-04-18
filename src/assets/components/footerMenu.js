3a7b2eae9d798fa3921b36e81a1ada4afb89d43a
MarkSuttirak <suttirak.ch@zaviago.com>
2023-04-18 09:36:08 +0700

    Update footerMenu.js

--- src/assets/components/footerMenu.js
+++ src/assets/components/footerMenu.js
@@ -6,28 +6,28 @@ import { Link } from "react-router-dom";
 
 const FooterMenu = (props) => {
     return (
-      <footer className="px-6 py-3 fixed w-full bg-white bottom-0 z-50" style={{borderTop:"1px solid #F8F8F8"}}>
+      <footer className="py-3 pr-2 fixed w-full bg-white bottom-0 z-50" style={{borderTop:"1px solid #F8F8F8"}}>
         <nav>
-          <ul className="flex justify-between">
-            <li className={`text-sm ${(props.activeMenu === 1 ? "primary-color" : "menu-inactive-color")}`}>
+          <ul className="block w-full">
+            <li className={`text-sm inline-block text-center w-1/4 ${(props.activeMenu === 1 ? "primary-color" : "menu-inactive-color")}`}>
               <Link to="/">
                 <FontAwesomeIcon icon={faHome} className="block text-xl m-auto"/>
                 <span className={`mt-1 inline-block ${(props.activeMenu === 1 ? "font-bold" : "font-medium")}`}>หน้าหลัก</span>
               </Link>
             </li>
-            <li className={`text-sm ${(props.activeMenu === 2 ? "primary-color" : "menu-inactive-color")}`}>
+            <li className={`text-sm inline-block text-center w-1/4 ${(props.activeMenu === 2 ? "primary-color" : "menu-inactive-color")}`}>
               <Link to="/rewards">
                 <FontAwesomeIcon icon={faGift} className="block text-xl m-auto"/>
                 <span className={`mt-1 inline-block ${(props.activeMenu === 2 ? "font-bold" : "font-medium")}`}>แลกของรางวัล</span>
               </Link>
             </li>
-            <li className={`text-sm ${(props.activeMenu === 3 ? "primary-color" : "menu-inactive-color")}`}>
+            <li className={`text-sm inline-block text-center w-1/4 ${(props.activeMenu === 3 ? "primary-color" : "menu-inactive-color")}`}>
               <Link to="/history">
                 <FontAwesomeIcon icon={faHistory} className="block text-xl m-auto"/>
                 <span className={`mt-1 inline-block ${(props.activeMenu === 3 ? "font-bold" : "font-medium")}`}>ประวัติ</span>
               </Link>
             </li>
-            <li className={`text-sm ${(props.activeMenu === 4 ? "primary-color" : "menu-inactive-color")}`}>
+            <li className={`text-sm inline-block text-center w-1/4 ${(props.activeMenu === 4 ? "primary-color" : "menu-inactive-color")}`}>
               <Link to="/my-account">
                 <FontAwesomeIcon icon={faUser} className="block text-xl m-auto"/>
                 <span className={`mt-1 inline-block ${(props.activeMenu === 4 ? "font-bold" : "font-medium")}`}>ข้อมูลของฉัน</span>
